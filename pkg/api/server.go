c1463c4a5da812de93955f1e1c24965e2f2375ab
Nikita99100 <nik-star99@mail.ru>
2021-09-17 03:08:52 +0300

    grpc log fix

--- pkg/api/server.go
+++ pkg/api/server.go
@@ -11,10 +11,10 @@ func Start(grpcS *grpc.Server, grpcServ FibonacciServer, addr string) {
 	RegisterFibonacciServer(grpcS, grpcServ)
 	grpcLis, err := net.Listen("tcp", fmt.Sprintf(":%v", addr))
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		log.Fatalf("gRPC failed to listen: %v", err)
 	}
-	log.Printf("server listening at %v", grpcLis.Addr())
+	log.Printf("gRPC server listening at %v", grpcLis.Addr())
 	if err := grpcS.Serve(grpcLis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+		log.Fatalf("gRPC failed to serve: %v", err)
 	}
 }
